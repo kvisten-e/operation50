3f43d717a7f6531dc7c292b8b78b902ec7f2f72c
MogenP12A <111068892+MogenP12A@users.noreply.github.com>
2023-06-16 09:22:55 +0200

    Update confetti.js

--- js/confetti.js
+++ js/confetti.js
@@ -1,1 +1,9 @@
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
+function party() {
+  confetti({
+
+  });
+}
+
+document.getElementById('startButton').addEventListener('click', party)
